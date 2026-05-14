b48c2fd4423cd503e7cb49d353a7ade9919a29db
JakubKap <jakubkapusta39@gmail.com>
2026-05-14 08:23:44 +0200

    test: restore original test file

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,19 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Flux.just("Jacob", "Anna", "Ron")
-						.filter(x -> x.startsWith("J"))
-								.subscribe(System.out::println);
-//		Mono.just()
-
 	}
 
 }
